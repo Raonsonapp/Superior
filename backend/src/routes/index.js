b2b7ad85525b1978c12748b2a6c843e63dcc5da3
Raonson <ehsonmahmadmurodov@gmail.com>
2026-03-05 23:07:46 +0500

    Create index.js

---
+++ backend/src/routes/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/videos", (req, res) => {
+  res.json({
+    videos: []
+  });
+});
+
+router.get("/health", (req, res) => {
+  res.json({
+    status: "ok"
+  });
+});
+
+module.exports = router;
