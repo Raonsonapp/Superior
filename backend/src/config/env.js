4ab6a56dfa92786faafbad9b2490d8d45ba5394a
Raonson <ehsonmahmadmurodov@gmail.com>
2026-03-05 23:04:37 +0500

    Update env.js

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -1,1 +1,11 @@
+require("dotenv").config();
 
+module.exports = {
+  PORT: process.env.PORT || 10000,
+  NODE_ENV: process.env.NODE_ENV || "development",
+
+  DATABASE_URL: process.env.DATABASE_URL || "",
+  REDIS_URL: process.env.REDIS_URL || "",
+
+  STORAGE_PATH: process.env.STORAGE_PATH || "./storage",
+};
