45823e90fb8d169d8b85f271a884bffd303f01c8
Raonson <ehsonmahmadmurodov@gmail.com>
2026-03-05 23:05:38 +0500

    Create storage.js

---
+++ backend/src/config/storage.js
@@ -0,0 +1,7 @@
+const path = require("path");
+
+module.exports = {
+  videoPath: path.join(__dirname, "../../storage/videos"),
+  thumbnailPath: path.join(__dirname, "../../storage/thumbnails"),
+  avatarPath: path.join(__dirname, "../../storage/avatars")
+};
