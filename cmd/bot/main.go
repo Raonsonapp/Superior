9f64f957306be9327dcf506f708b98cc9b6ca9e9
Hyperion <ehsonmahmadmurodov@gmail.com>
2026-06-27 15:50:05 +0500

    Update main.go

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -5,32 +5,20 @@ import (
 	"log"
 	"net/http"
 	"os"
-
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 func main() {
-	token := os.Getenv("BOT_TOKEN")
-
-	if token == "" {
-		log.Fatal("BOT_TOKEN is missing")
-	}
-
-	bot, err := tgbotapi.NewBotAPI(token)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Printf("Bot started: @%s\n", bot.Self.UserName)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Superior Bot is running 🚀")
+		fmt.Fprintln(w, "Superior Bot Running 🚀")
 	})
 
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "7860"
 	}
+
+	log.Println("Server started on :" + port)
 
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
